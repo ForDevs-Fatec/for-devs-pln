f36101af398008492d94a1b79e3edcb941d49771
bruno-pisciotta281 <bruno.pisciotta43@gmail.com>
2023-10-09 20:49:04 -0300

    Atualização da pipeline de Stopwords com lista de palavras

--- pipeline/pipeline_Stopwords.py
+++ pipeline/pipeline_Stopwords.py
@@ -1,29 +1,41 @@
 import pandas as pd
 import nltk
-from nltk.corpus import stopwords
 nltk.download('stopwords')
 
-# Carregando as stopwords em português
-stop_words = set(stopwords.words('portuguese'))
+# lista personalizada de stopwords
+stop_words_custom = set([
+    'a', 'o', 'os', 'as', 'um', 'uma', 'uns', 'umas', 'de', 'do', 'da', 'dos', 'das', 'em', 'para', 'por', 'com', 'sem', 'e', 'mas',
+    'ou', 'que', 'se', 'quando', 'onde', 'como', 'porquê', 'quem', 'isso', 'aquilo', 'este', 'esse', 'aquele', 'ela', 'ele', 'eles',
+    'elas', 'nós', 'vós', 'vocês', 'eu', 'você', 'tu', 'muito', 'pouco', 'bom', 'ruim', 'bonito', 'feio', 'grande', 'pequeno', 'novo',
+    'velho', 'caro', 'barato', 'fácil', 'difícil', 'rápido', 'lento', 'perto', 'longe', 'aqui', 'ali', 'lá', 'agora', 'depois', 'ontem',
+    'hoje', 'amanhã', 'muito', 'pouco', 'bom', 'ruim', 'bonito', 'feio', 'grande', 'pequeno', 'novo', 'velho', 'caro', 'barato',
+    'fácil', 'difícil', 'rápido', 'lento', 'perto', 'longe', 'aqui', 'ali', 'lá', 'agora', 'depois', 'ontem', 'hoje', 'amanhã',
+    'de', 'acordo', 'com', 'primeiro', 'segundo', 'terceiro', 'lugar', 'no', 'entanto', 'outro', 'além', 'disso', 'concluindo',
+    'fim', 'que', 'a', 'partir', 'até', 'desde', 'durante', 'antes', 'depois', 'que', 'assim', 'então', 'portanto', 'dessa',
+    'forma', 'dessa', 'maneira', 'dessa', 'forma', 'dessa', 'maneira', 'eu', 'você', 'ele', 'ela', 'nós', 'vós', 'vocês',
+    'eles', 'elas', 'meu', 'seu', 'seu', 'dela', 'nosso', 'vosso', 'vosso', 'deles', 'delas', 'este', 'esse', 'aquele',
+    'esta', 'essa', 'aquela', 'aquilo', 'isso', 'isso', 'o', 'que', 'quem', 'como', 'quando', 'onde', 'por', 'que', 'qual',
+    'quanto', 'se', 'mas', 'ou', 'e', 'porque', 'pois', 'por', 'isso', 'logo', 'portanto', 'assim', 'então'
+])
 
-# Link para o CSV
+# Link do CSV
 csv_url = 'https://raw.githubusercontent.com/americanas-tech/b2w-reviews01/master/B2W-Reviews01.csv'
 
-# Lendo o CSV para um DataFrame do pandas
+# Lendo o CSV pro df do pandas
 df = pd.read_csv(csv_url)
 
-# Colunas do DataFrame que contêm texto para processamento
+# Colunas do DataFrame com o texto
 colunas_texto = ['review_title', 'review_text']
 
 # Função para remover stopwords
 def remover_stopwords(texto):
     palavras = str(texto).split()
-    palavras_sem_stopwords = [palavra for palavra in palavras if palavra.lower() not in stop_words]
+    palavras_sem_stopwords = [palavra for palavra in palavras if palavra.lower() not in stop_words_custom]
     return ' '.join(palavras_sem_stopwords)
 
-# Aplicar a remoção de stopwords às colunas de texto
+# Aplicar a remoção das stops
 for coluna in colunas_texto:
     df[coluna] = df[coluna].apply(remover_stopwords)
 
-# Exibir o DataFrame com as stopwords removidas
+# Exibir o bagulho
 print(df)
