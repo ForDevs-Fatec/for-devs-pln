ebd85c95907a02b673371bd8b15a9b348c9cf19f
Ana Carolina dos Santos Rocha <78958795+annakks@users.noreply.github.com>
2023-11-06 02:58:10 -0300

    fix: Melhoria na Analise de Sentimento

--- pipeline/pipeline_analiseSentimento.py
+++ pipeline/pipeline_analiseSentimento.py
@@ -1,120 +1,78 @@
-from textblob import TextBlob
-from sklearn.neural_network import MLPClassifier
-from gensim.models import word2vec
+import re
 import numpy as np
-import pandas as pd
+from numpy import dot
+from numpy.linalg import norm
+from gensim.models import word2vec
+from nltk.tokenize import word_tokenize
+from sklearn.metrics.pairwise import cosine_similarity
 
-def analisar(review_text, texto, model_word2vec):
-    classified_reviews= [
-    {'corpus': "adorei o produto", 'review_type': 'positive', 'feature_vector': []},
-    {'corpus': "amei o produto ", 'review_type': 'positive', 'feature_vector': []},
-    {'corpus': "produto excelente", 'review_type': 'positive', 'feature_vector': []},
-    {'corpus': "com certeza comprarei novamente ", 'review_type': 'positive', 'feature_vector': []},
-    {'corpus': "produto muito bom", 'review_type': 'positive', 'feature_vector': []},
-    {'corpus': "atende as expectativas", 'review_type': 'positive', 'feature_vector': []},
-    {'corpus': 'consumidor amou o produto', 'review_type': 'positive', 'feature_vector': []},
-    {'corpus': 'recomendo a todos', 'review_type': 'positive', 'feature_vector': []},
-    {'corpus': 'entrega no prazo', 'review_type': 'positive', 'feature_vector': []},
-    {'corpus': "comprarei de novo", 'review_type': 'positive', 'feature_vector': []},
-    {'corpus': "produto maravilhoso", "review_type": 'positive', 'feature_vector': []},
-    {'corpus': "produto de parabéns", "review_type": 'positive', 'feature_vector': []},
-    {'corpus': "super recomendo excelente", "review_type": 'positive', 'feature_vector': []},
-    {'corpus': "excelente qualidade", "review_type": 'positive', 'feature_vector': []},
-    {'corpus': "produto otimo", "review_type": 'positive', 'feature_vector': []},
+def analisar(review_text, texto):
 
-    {'corpus': "detestei o produto ", 'review_type': 'negative', 'feature_vector': []},
-    {'corpus': "produto horrível ", 'review_type': 'negative', 'feature_vector': []},
-    {'corpus': "nunca mais compro novamente ", 'review_type': 'negative', 'feature_vector': []},
-    {'corpus': "produto não foi entregue", "review_type": 'negative', 'feature_vector': []},
-    {'corpus': "entrega atrasou", "review_type": 'negative', 'feature_vector': []},
-    {'corpus': "produto com problemas", "review_type": 'negative', 'feature_vector': []},
-    {'corpus': "produto não funciona", "review_type": 'negative', 'feature_vector': []},
-    {'corpus': "pior produto que comprei ", 'review_type': 'negative', 'feature_vector': []},
-    {'corpus': "não é bom", "review_type": 'negative', 'feature_vector': []},
-    {'corpus': "não recomendo", "review_type": 'negative', 'feature_vector': []},
-    {'corpus': "produto péssimo", "review_type": 'negative', 'feature_vector': []},
-    {'corpus': "não recebi", 'review_type': 'negative', 'feature_vector': []},
-
-    {'corpus': "produto satisfatório", 'review_type': 'neutral', 'feature_vector': []},
-    {'corpus': "produto atende as necessidades", 'review_type': 'neutral', 'feature_vector': []},
-    {'corpus': "produto ok", 'review_type': 'neutral', 'feature_vector': []},
-    {'corpus': "produto razoável", 'review_type': 'neutral', 'feature_vector': []},
-    {'corpus': "não informado", 'review_type': 'neutral', 'feature_vector': []},
+    setencas_politivas= ["adorei o produto",
+                         "amei o produto", "O produto atendeu as expectativas. Até o momento não tive nenhum problema.",
+                         "Excelente produto, entrega super rápida. Recomendo a todos", 
+                         "com certeza comprarei novamente ", "Produto com bom custo benefício.. boa qualidade, estampas bem fiéis as da foto. Não tive problemas após lavagem,"
+                         "produto muito bom", "Produto de boa qualidade, superou minhas expectativas", 
+                         "consumidor amou o produto", "recomendo a todos", "O produto funciona perfeitamente, muito bom e muito prático de instalação e uso, eu o recomendo a todos, e muito barato e muito bom"
+                         "entrega no prazo", "comprarei de novo", "produto maravilhoso", 
+                         "Comprei exatamente o que queria, excelente produto, altíssima qualidade, super recomendo", 
+                         "excelente qualidade", "produto otimo", "O aparelho chegou em perfeito estado e antes do previsto"]
     
-    ]
+    setencas_negativas= ["detestei o produto ", "produto horrível ", "nunca mais compro novamente ", "produto não foi entregue", "entrega atrasou", "não chegou","Até agora não recebi o meu pedido","produto com problemas, quebrou e não compensava arrumar", "Produto não condiz com as informações da embalagem",
+                         "Não funciona, o nome é modelador de cachos, mas cachos que é bom não faz, veio na cor errada. E além de tudo isso não respondem as mensagens e não fazem o cancelamento ou a troca.", 
+                         "pior produto que comprei ", "não é bom",   "não recomendo", "produto péssimo", "Não recebi o produto até a presente data, Lamentável isso! Vou denunciar no procon!", "O fone de ouvido não é original, qualidade péssima e som abafado.",
+                         "não gostei ninguém resolve nada Já liguei várias vezes e ninguém resolve nada e eu quero meu dinheiro de volta", "Produto de péssima qualidade, não tem nada a ver com a foto", "O preço é enganoso" ]
+    
+    setencas_neutras= ["produto satisfatório", "produto atende as necessidades", "produto ok", "produto razoável", "não informado", "Atende as necessidades e expectativa do usuário, com preço acessível e de acordo com minhas disponibilidade.",
+                       "Está de acordo com esperado.chegou dentro do praso pode comprar", "Por enquanto, sem queixas. Se surgir algum problema, com certeza voltarei para registrar.",
+                       "Fiquei satisfeito com relação ao produto, prazo de entrega e preço."]
 
-    documento = texto
-    unclassified_review = {
-    'corpus': documento,
-    'review_type': '',
-    'feature_vector': []
-    }
+    positive_sentences = [word_tokenize(sentence) for sentence in setencas_politivas]
+    negative_sentences = [word_tokenize(sentence) for sentence in setencas_negativas]
+    neutral_sentences = [word_tokenize(sentence) for sentence in setencas_neutras]
 
-    base_model_lexicon = ['a', 'ajuda', 'ajudar', 'confusão', 'bom', 'mau',
-                    'atendimento', 'como', 'confuso',
-                    'consigo', 'de', 'desejo', 'encerrar',
-                    'estou', 'favor', 'gostaria', 'há', 'mais',
-                    'me', 'não', 'obter', 'opção', 'outra', 'poderia',
-                    'por', 'qual', 'sei', 'é', 'essa']
-
-    # words used as inputs shall be those resulted from tokenization proccess and other
-    # preprocessing steps.
-    def build_model_lexicon(words, model_lexicon):
-        for word in words:
-            if word not in model_lexicon:
-                model_lexicon.append(word)
-        model_lexicon.sort()
-
-    def build_feature_vector(words, model_lexicon, model_word2vec):
-        bag_of_words_count = np.zeros(len(model_lexicon))
-        for pos in range(len(model_lexicon)):
-            for word in words:
-                if word == model_lexicon[pos]:
-                    if word in model_word2vec.wv:
-                        bag_of_words_count[pos] += 1
-        return bag_of_words_count
-
-
-    # Here we build the model lexicon
-    for classified_review in classified_reviews:
-        build_model_lexicon(classified_review['corpus'].split(), base_model_lexicon)
-    build_model_lexicon(unclassified_review['corpus'].split(), base_model_lexicon)
-
-    # Now we extract the feature vector considering the model
-    for classified_review in classified_reviews:
-        classified_review['feature_vector'] = build_feature_vector(classified_review['corpus'].split(), base_model_lexicon)
-
-    unclassified_review['feature_vector'] = build_feature_vector(unclassified_review['corpus'].split(), base_model_lexicon, model_word2vec)
-
-    X = [] # feature vectors
-    y = [] # feature classes
-    for review in classified_reviews:
-        X.append(review['feature_vector'])
-        y.append(review['review_type'])
-
-    classifier = MLPClassifier(solver='lbfgs', alpha=1e-5,
-                        hidden_layer_sizes=(5, 3), random_state=1)
-
-    classifier.fit(X, y)
-
-    sentimento = classifier.predict([unclassified_review['feature_vector']])
-
-    print(review_text)
-    print(sentimento[0])
-    print('==================================================================')
-    return sentimento[0]
-
-def executar_analise_sentimento(df):
-
-    feature_size = 5  
+    feature_size = 5  # size of vector representation
     window_context = 2
     min_word_count = 1
     sample = 1e-3
 
-    model_word2vec = word2vec.Word2Vec(df['review_text_normalized'], vector_size=feature_size,
-                                window=window_context, min_count=min_word_count,
-                                sample=sample, epochs=50)
+    w2vec_model_positivo = word2vec.Word2Vec(positive_sentences, vector_size= feature_size,
+                                    window=window_context, min_count= min_word_count,
+                                    sample=sample, epochs = 50)
+    w2vec_model_negativo = word2vec.Word2Vec(negative_sentences, vector_size= feature_size,
+                                    window=window_context, min_count= min_word_count,
+                                    sample=sample, epochs = 50)
+    w2vec_model_neutro = word2vec.Word2Vec(neutral_sentences, vector_size= feature_size,
+                                    window=window_context, min_count= min_word_count,
+                                    sample=sample, epochs = 50)
     
-    df['sentiment_result'] = df.apply(lambda row: analisar(row['review_text'], row['review_text_normalized'], model_word2vec), axis=1)
-    
+    sentence_vector = np.zeros(feature_size)
+    positivo_similarity = []
+    negativo_similarity = []
+    neutro_similarity = []
+
+    for word in texto.split():
+        if word in w2vec_model_positivo.wv:
+            positivo_similarity += cosine_similarity([sentence_vector], [w2vec_model_positivo.wv[word]])[0][0]
+        if word in w2vec_model_negativo.wv:
+            negativo_similarity += cosine_similarity([sentence_vector], [w2vec_model_negativo.wv[word]])[0][0]
+        if word in w2vec_model_neutro.wv:
+            neutro_similarity += cosine_similarity([sentence_vector], [w2vec_model_neutro.wv[word]])[0][0]
+
+    if positivo_similarity > negativo_similarity and positivo_similarity > neutro_similarity:
+        print(review_text)
+        print('==================================================================')
+        return "positiva"
+    elif negativo_similarity > positivo_similarity and negativo_similarity > neutro_similarity:
+        print(review_text)
+        print('==================================================================')
+        return "negativa"
+    else:
+        print(review_text)
+        print('==================================================================')
+        return "neutra"
+
+def executar_analise_sentimento(df):
+
+    df['sentiment_result'] = df.apply(lambda row: analisar(row['review_text'], row['review_text_normalized']), axis=1)
     return df
