7c400b891a765d9c565a50cb4a41da28646ce98d
Ana Carolina dos Santos Rocha <78958795+annakks@users.noreply.github.com>
2023-11-15 20:49:37 -0300

    Fix: correção da função de correção ortografica

--- funcoes/correcao_ortografica.py
+++ funcoes/correcao_ortografica.py
@@ -11,25 +11,20 @@ def remover_duplicidade(texto):
             texto_sem_duplicidade += texto[i]
             quantidade_duplicados_consoantes = 0
         else:
-            if texto[i] not in vogais and quantidade_duplicados_consoantes < 2:
-                texto_sem_duplicidade += texto[i]
+            if texto[i] not in vogais:
                 quantidade_duplicados_consoantes += 1
+                if quantidade_duplicados_consoantes < 2:
+                    texto_sem_duplicidade += texto[i]
+            else:
+                texto_sem_duplicidade += texto[i]
+
     return texto_sem_duplicidade
 
-def corrigir(texto):
+def corrigir_palavra(word):
     portuguese = SpellChecker(language='pt')
-    texto_corrigido = [portuguese.correction(word) for word in texto]
-    return texto_corrigido
+    return portuguese.correction(word)
 
-#def corrigir_textos(df):
-#    df['review_text_normalized'] = df['review_text_normalized'].apply(remover_duplicidade)
-#    df['review_text_normalized'] = df['review_text_normalized'].apply(corrigir)
-#    return df
-
-texto = ['sempree', 'compro', 'Americanass', 'recomemdo', 'loja', 'amigos', 'bom', 'demaisss']
-
-dados_sem_duplicidade = [remover_duplicidade(palavra) for palavra in texto]
-dados_corrigidos = corrigir(dados_sem_duplicidade)
-
-print("Palavras sem duplicidade:", dados_sem_duplicidade)
-print("Palavras corrigidas:", dados_corrigidos)
+def corrigir_textos(df):
+    df['review_text_normalized'] = df['review_text_normalized'].apply(lambda x: [remover_duplicidade(word) for word in x])
+    df['review_text_normalized'] = df['review_text_normalized'].apply(lambda x: [corrigir_palavra(word) for word in x])
+    return df
